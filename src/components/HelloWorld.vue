87165f9fbec435064ac242bfff295680568f3176
陈艺平 <2827023192@qq.com>
2026-04-03 16:05:41 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,4 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8920599655980690"
-     crossorigin="anonymous"></script>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 import viteLogo from '../assets/vite.svg'
